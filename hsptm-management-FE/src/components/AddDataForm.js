09665c646d0834d3d11976dbe84c2c248c411ddf
hassan zaidi <syed.hassan.raza677@gmail.com>
2024-01-29 17:07:28 +0500

    updated addDataForm with mui components

--- hsptm-management-FE/src/components/AddDataForm.js
+++ hsptm-management-FE/src/components/AddDataForm.js
@@ -1,32 +1,30 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
 
-import { Modal, Box } from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
-import { toast, ToastContainer } from "react-toastify";
+import '../AddDataForm.css';
 
-import "react-toastify/dist/ReactToastify.css";
+import {Modal, Box, Typography, TextField, Button} from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import {toast, ToastContainer} from 'react-toastify';
 
-import "../AddDataForm.css";
+function AddDataForm({setOpenAddDataForm, openAddDataForm, darkMode}) {
+  const [seriennr, setSeriennr] = useState('');
+  const [gehortzu, setGehortzu] = useState('');
+  const [anlagenID, setAnlagenID] = useState('');
+  const [anlagenbez, setAnlagenbez] = useState('');
+  const [typModell, setTypModell] = useState('');
+  const [hersteller, setHersteller] = useState('');
+  const [lieferant, setLieferant] = useState('');
+  const [servicestelle, setServicestelle] = useState('');
+  const [abteilung, setAbteilung] = useState('');
+  const [kostenstelle, setKostenstelle] = useState('');
+  const [SLA, setSLA] = useState('');
+  const [preisProSLA, setpreisProSLA] = useState('');
+  const [status, setStatus] = useState('');
+  const [raumbezMT, setRaumbezMT] = useState('');
 
-import axios from "axios";
-
-function AddDataForm({ setOpenAddDataForm, openAddDataForm, darkMode }) {
-  const [seriennr, setSeriennr] = useState("");
-  const [gehortzu, setGehortzu] = useState("");
-  const [anlagenID, setAnlagenID] = useState("");
-  const [anlagenbez, setAnlagenbez] = useState("");
-  const [typModell, setTypModell] = useState("");
-  const [hersteller, setHersteller] = useState("");
-  const [lieferant, setLieferant] = useState("");
-  const [servicestelle, setServicestelle] = useState("");
-  const [abteilung, setAbteilung] = useState("");
-  const [kostenstelle, setKostenstelle] = useState("");
-  const [SLA, setSLA] = useState("");
-  const [preisProSLA, setpreisProSLA] = useState("");
-  const [status, setStatus] = useState("");
-  const [raumbezMT, setRaumbezMT] = useState("");
-
-  const hanldleSumitForm = async () => {
+  const handleSumitForm = async () => {
     try {
       const reqData = {
         seriennr,
@@ -42,20 +40,17 @@ function AddDataForm({ setOpenAddDataForm, openAddDataForm, darkMode }) {
         SLA,
         preisProSLA,
         status,
-        raumbezMT,
+        raumbezMT
       };
       if (!reqData.anlagenID) {
-        return toast.error("Anlagen-ID Should not be empty");
+        return toast.error('Anlagen-ID Should not be empty');
       }
-      const response = await axios.post(
-        "http://localhost:8080/api/devices",
-        reqData
-      );
+      const response = await axios.post('http://localhost:8080/api/devices', reqData);
 
       setOpenAddDataForm(false);
-      !!response.data && toast.success("Data is added successfully");
+      !!response.data && toast.success('Data is added successfully');
     } catch (error) {
-      toast.error("Something went wrong");
+      toast.error('Something went wrong');
       console.log(error);
     }
   };
@@ -63,65 +58,61 @@ function AddDataForm({ setOpenAddDataForm, openAddDataForm, darkMode }) {
   return (
     <Modal
       open={openAddDataForm}
-      // onClose={handleClose}
       aria-labelledby="modal-title"
       aria-describedby="modal-description"
       sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
       }}
     >
-      <div
-        className={darkMode ? "dark-mode data-form" : "light-mode data-form"}
-      >
-        <ToastContainer
-          position="top-center"
-          autoClose={3000}
-          hideProgressBar
-        />
+      <Box className={darkMode ? 'dark-mode data-form' : 'light-mode data-form'}>
+        <ToastContainer position="top-center" autoClose={3000} hideProgressBar />
         <Box
           sx={{
-            display: "flex",
-            justifyContent: "flex-end",
-            alignItems: "center",
-            "&:hover svg": {
-              cursor: "pointer",
-              color: "red", // Change the color to your desired color
-            },
+            display: 'flex',
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+            '&:hover svg': {
+              cursor: 'pointer',
+              color: 'red'
+            }
           }}
           onClick={() => setOpenAddDataForm(false)}
         >
           <CloseIcon />
         </Box>
-        <h2 className="title">Add Data in Device</h2>
-        <div className="form-container">
-          <div className="row">
-            <div className="input-container">
-              <label htmlFor="field1">ANLAGEN-ID</label>
-              <input
-                onChange={(e) => setAnlagenID(e.target.value)}
+        <Typography variant="h5" className="title">
+          Add Data in Device
+        </Typography>
+        <Box className="form-container">
+          <Box className="row">
+            <Box className="input-container">
+              <TextField
+                fullWidth
+                label="ANLAGEN-ID"
+                name="anlagenID"
                 value={anlagenID}
-                type="text"
-                id="field1"
-                className="text-input"
+                onChange={(e) => setAnlagenID(e.target.value)}
+                margin="normal"
                 placeholder="ID"
               />
-            </div>
-            <div className="input-container">
-              <label htmlFor="field2">SERIENNR.</label>
-              <input
-                onChange={(e) => setSeriennr(e.target.value)}
-                value={seriennr}
+            </Box>
+            <Box className="input-container">
+              <TextField
+                fullWidth
+                label="SERIENNR."
+                name="seriennr"
                 type="text"
-                id="field2"
-                className="text-input"
+                value={seriennr}
+                onChange={(e) => setSeriennr(e.target.value)}
+                margin="normal"
                 placeholder="Sr.No."
               />
-            </div>
-          </div>
-          <div className="row">
-            <div className="input-container">
+            </Box>
+          </Box>
+          <Box className="row">
+            <Box className="input-container">
               <label htmlFor="field3">Gehort zu</label>
               <input
                 value={gehortzu}
@@ -131,8 +122,8 @@ function AddDataForm({ setOpenAddDataForm, openAddDataForm, darkMode }) {
                 className="text-input"
                 placeholder="TYPE"
               />
-            </div>
-            <div className="input-container">
+            </Box>
+            <Box className="input-container">
               <label htmlFor="field4">Anlagenbez</label>
               <input
                 onChange={(e) => setAnlagenbez(e.target.value)}
@@ -142,10 +133,10 @@ function AddDataForm({ setOpenAddDataForm, openAddDataForm, darkMode }) {
                 className="text-input"
                 placeholder="TYPE"
               />
-            </div>
-          </div>
-          <div className="row">
-            <div className="input-container">
+            </Box>
+          </Box>
+          <Box className="row">
+            <Box className="input-container">
               <label htmlFor="field5">Typ/Model</label>
               <input
                 onChange={(e) => setTypModell(e.target.value)}
@@ -155,8 +146,8 @@ function AddDataForm({ setOpenAddDataForm, openAddDataForm, darkMode }) {
                 className="text-input"
                 placeholder="TYPE"
               />
-            </div>
-            <div className="input-container">
+            </Box>
+            <Box className="input-container">
               <label htmlFor="field6">Hersteller</label>
               <input
                 onChange={(e) => setHersteller(e.target.value)}
@@ -166,10 +157,10 @@ function AddDataForm({ setOpenAddDataForm, openAddDataForm, darkMode }) {
                 className="text-input"
                 placeholder="TYPE"
               />
-            </div>
-          </div>
-          <div className="row">
-            <div className="input-container">
+            </Box>
+          </Box>
+          <Box className="row">
+            <Box className="input-container">
               <label htmlFor="field7">Lieferant</label>
               <input
                 value={lieferant}
@@ -179,8 +170,8 @@ function AddDataForm({ setOpenAddDataForm, openAddDataForm, darkMode }) {
                 className="text-input"
                 placeholder="TYPE"
               />
-            </div>
-            <div className="input-container">
+            </Box>
+            <Box className="input-container">
               <label htmlFor="field8">Servicestelle</label>
               <input
                 onChange={(e) => setServicestelle(e.target.value)}
@@ -190,10 +181,10 @@ function AddDataForm({ setOpenAddDataForm, openAddDataForm, darkMode }) {
                 className="text-input"
                 placeholder="TYPE"
               />
-            </div>
-          </div>
-          <div className="row">
-            <div className="input-container">
+            </Box>
+          </Box>
+          <Box className="row">
+            <Box className="input-container">
               <label htmlFor="field7">Abteilung</label>
               <input
                 onChange={(e) => setAbteilung(e.target.value)}
@@ -203,75 +194,67 @@ function AddDataForm({ setOpenAddDataForm, openAddDataForm, darkMode }) {
                 className="text-input"
                 placeholder="TYPE"
               />
-            </div>
-            <div className="input-container">
-              <label htmlFor="field8">Kostenstelle</label>
-              <input
-                onChange={(e) => setKostenstelle(e.target.value)}
+            </Box>
+            <Box className="input-container">
+              <TextField
+                fullWidth
+                label="Kostenstelle"
                 value={kostenstelle}
-                type="text"
-                id="field8"
-                className="text-input"
+                onChange={(e) => setKostenstelle(e.target.value)}
+                margin="normal"
                 placeholder="TYPE"
               />
-            </div>
-          </div>
-          <div className="row">
-            <div className="input-container">
-              <label htmlFor="field7">SLA</label>
-              <input
-                onChange={(e) => setSLA(e.target.value)}
+            </Box>
+          </Box>
+          <Box className="row">
+            <Box className="input-container">
+              <TextField
+                fullWidth
+                label="SLA"
                 value={SLA}
-                type="text"
-                id="field7"
-                className="text-input"
+                onChange={(e) => setSLA(e.target.value)}
+                margin="normal"
                 placeholder="TYPE"
               />
-            </div>
-            <div className="input-container">
-              <label htmlFor="field8">Preis pro SLA</label>
-              <input
-                onChange={(e) => setpreisProSLA(e.target.value)}
+            </Box>
+            <Box className="input-container">
+              <TextField
+                fullWidth
+                label="Preis pro SLA"
                 value={preisProSLA}
-                type="text"
-                id="field8"
-                className="text-input"
+                onChange={(e) => setpreisProSLA(e.target.value)}
+                margin="normal"
                 placeholder="TYPE"
               />
-            </div>
-          </div>
-          <div className="row">
-            <div className="input-container">
-              <label htmlFor="field7">Status</label>
-              <input
-                onChange={(e) => setStatus(e.target.value)}
+            </Box>
+          </Box>
+          <Box className="row">
+            <Box className="input-container">
+              <TextField
+                fullWidth
+                label="Status"
                 value={status}
-                type="text"
-                id="field7"
-                className="text-input"
+                onChange={(e) => setStatus(e.target.value)}
+                margin="normal"
                 placeholder="TYPE"
               />
-            </div>
-            <div className="input-container">
-              <label htmlFor="field8">Raumbez. MT</label>
-              <input
-                onChange={(e) => setRaumbezMT(e.target.value)}
+            </Box>
+            <Box className="input-container">
+              <TextField
+                fullWidth
+                label="Raumbez. MT"
                 value={raumbezMT}
-                type="text"
-                id="field8"
-                className="text-input"
+                onChange={(e) => setRaumbezMT(e.target.value)}
+                margin="normal"
                 placeholder="TYPE"
               />
-            </div>
-          </div>
-          <button className="submit-button" onClick={hanldleSumitForm}>
+            </Box>
+          </Box>
+          <Button variant="contained" color="primary" className="submit-button" onClick={handleSumitForm}>
             Submit
-          </button>
-          {/* <button className="submit-button" variant="contained" onClick={()=>setOpenAddDataForm(false)}>
-           Close
-          </button> */}
-        </div>
-      </div>
+          </Button>
+        </Box>
+      </Box>
     </Modal>
   );
 }
